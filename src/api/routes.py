bd78c1a5888fccd38f466edb84eaa30c8a7b6518
MireyaCR <mireya.mcr@gmail.com>
2023-02-28 18:19:03 +0000

    create login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,21 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    
+    user= User.query.filter_by(email=data['email'],password=data['password']).first()
+    # user= User.query.filter_by(email=data.get['email'],password=data.get['password']).first()
+    if user:
+        return jsonify(data), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    return jsonify({"message":"Usuario/Contraseña incorrectos"}),400
 
-    return jsonify(response_body), 200
