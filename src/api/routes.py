2402fbe73a798f64ecb5034b10bfc41b73ebd3f4
MireyaCR <mireya.mcr@gmail.com>
2023-02-28 18:41:00 +0000

    send token

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token, jwt_required,get_jwt_identity
+
+
 
 api = Blueprint('api', __name__)
 
@@ -23,3 +23,9 @@ def login():
 
     return jsonify({"message":"Usuario/Contraseña incorrectos"}),400
 
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def get_user():
+    user_id=get_jwt_identity()
+    user=User.query.filter_by(id=user_id).first()
+    return jsonify(user.serialize()),200
